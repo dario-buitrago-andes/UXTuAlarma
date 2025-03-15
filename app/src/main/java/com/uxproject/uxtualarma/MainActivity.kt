063cb485ae5d43a8094e40e9689ae091c5e3bd31
Juan Manuel González Garzón <jmgonzalezgarzon@gmail.com>
2025-03-14 23:43:55 -0500

    Lista y detalle de alarma

--- app/src/main/java/com/uxproject/uxtualarma/MainActivity.kt
+++ app/src/main/java/com/uxproject/uxtualarma/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.uxproject.uxtualarma
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -17,18 +21,122 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            UXTuAlarmaTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.ingresar)
+        button.setOnClickListener {
+            val intent = Intent(this, AlarmListActivity::class.java)
+            startActivity(intent)
         }
     }
 }
+
+class AlarmListActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.alarm_list)
+
+        val buttonAccount = findViewById<ImageView>(R.id.imageView10)
+        buttonAccount.setOnClickListener {
+            val intent = Intent(this, AccountActivity::class.java)
+            startActivity(intent)
+        }
+
+        val buttonMas = findViewById<ImageView>(R.id.imageView12)
+        buttonMas.setOnClickListener {
+            val intent = Intent(this, MasActivity::class.java)
+            startActivity(intent)
+        }
+
+        val buttonAlarms = findViewById<ImageView>(R.id.imageView11)
+        buttonAlarms.setOnClickListener {
+            val intent = Intent(this, AlarmListActivity::class.java)
+            startActivity(intent)
+        }
+
+        val itemTrabajo = findViewById<TextView>(R.id.textView)
+        itemTrabajo.setOnClickListener {
+            val intent = Intent(this, AlarmDetailActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
+
+class AlarmDetailActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.alarm_detail)
+
+        val buttonAccount = findViewById<ImageView>(R.id.imageView10)
+        buttonAccount.setOnClickListener {
+            val intent = Intent(this, AccountActivity::class.java)
+            startActivity(intent)
+        }
+
+        val buttonMas = findViewById<ImageView>(R.id.imageView12)
+        buttonMas.setOnClickListener {
+            val intent = Intent(this, MasActivity::class.java)
+            startActivity(intent)
+        }
+
+        val buttonAlarms = findViewById<ImageView>(R.id.imageView11)
+        buttonAlarms.setOnClickListener {
+            val intent = Intent(this, AlarmListActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
+
+class MasActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.mas_screen)
+
+        val buttonAccount = findViewById<ImageView>(R.id.imageView10)
+        buttonAccount.setOnClickListener {
+            val intent = Intent(this, AccountActivity::class.java)
+            startActivity(intent)
+        }
+
+        val buttonMas = findViewById<ImageView>(R.id.imageView12)
+        buttonMas.setOnClickListener {
+            val intent = Intent(this, MasActivity::class.java)
+            startActivity(intent)
+        }
+
+        val buttonAlarms = findViewById<ImageView>(R.id.imageView11)
+        buttonAlarms.setOnClickListener {
+            val intent = Intent(this, AlarmListActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
+
+class AccountActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.account_screen)
+
+        val buttonAccount = findViewById<ImageView>(R.id.imageView10)
+        buttonAccount.setOnClickListener {
+            val intent = Intent(this, AccountActivity::class.java)
+            startActivity(intent)
+        }
+
+        val buttonMas = findViewById<ImageView>(R.id.imageView12)
+        buttonMas.setOnClickListener {
+            val intent = Intent(this, MasActivity::class.java)
+            startActivity(intent)
+        }
+
+        val buttonAlarms = findViewById<ImageView>(R.id.imageView11)
+        buttonAlarms.setOnClickListener {
+            val intent = Intent(this, AlarmListActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
